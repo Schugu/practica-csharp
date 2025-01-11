a4868531e24043826b5b41c6cdf5fc5f510d764c
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2025-01-11 16:49:40 -0300

    Add: Multiplication table

---
+++ multiplication-table/multiplication-table/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Tabla de multiplicar");
+Console.Write("Ingrese el numero a multiplicar: ");
+var input = Console.ReadLine();
+
+if (float.TryParse(input, out float factor))
+{
+    Console.Write("Ingrese hasta que número multiplicar: ");
+    var input2 = Console.ReadLine();
+
+    if (float.TryParse(input2, out float multiplo))
+    {
+        Console.WriteLine($"Tabla de multiplicar de: {factor}");
+        for (int i = 0; i <= multiplo; i++)
+        {
+            Console.WriteLine($"{factor} x {i} = {factor * i}");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Entrada no válida.");
+    }
+}
+else
+{
+    Console.WriteLine("Entrada no válida.");
+}
