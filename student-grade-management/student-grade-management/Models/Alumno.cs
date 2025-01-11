9e050b388719f933ea4ff00aa8461c276263d48b
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2025-01-11 19:00:30 -0300

    Add: student-grade-management

---
+++ student-grade-management/student-grade-management/Models/Alumno.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace student_grade_management.Models
+{
+    class Alumno
+    {
+        public string Name { get; set; }
+
+        public List<float> grades = new();
+
+        public string addName()
+        {
+            Console.Write("Ingrese el nombre del estudiante: ");
+            return Console.ReadLine();
+        } 
+
+        public List<float> addGrade(int numPruebas)
+        {
+            Console.WriteLine("Ingrese las notas de las pruebas: ");
+
+            List<float> newGreades = new();
+
+            for (int i = 1; i <= numPruebas; i++)
+            {
+                Console.Write($"Prueba Nro{i}: ");
+                var inputGrade = Console.ReadLine();
+                if (float.TryParse(inputGrade, out float result))
+                {
+                    newGreades.Add(result);
+                }
+                else
+                {
+                    Console.WriteLine("Entrada no válida.");
+                }
+
+            }
+
+            return newGreades;
+        }
+
+        public float GetAverage()
+        {
+            float gradesSumados = 0;
+            foreach (float grade in grades)
+            {
+                gradesSumados += grade;
+            }
+
+
+
+            return gradesSumados / grades.Count();
+        }
+    }
+}
---
+++ student-grade-management/student-grade-management/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using student_grade_management.Models;
+
+
+class Program
+{
+    static void Main()
+    {
+        var alumno = new Alumno();
+
+        List<Alumno> alumnos = new List<Alumno>();
+
+        Console.WriteLine("Gestión de notas de estudiantes");
+        Console.Write("Ingrese el número de pruebas realizadas: ");
+        dynamic numPruebas = Console.ReadLine();
+
+        if (int.TryParse(numPruebas, out int result))
+        {
+            numPruebas = result;
+        }
+        else
+        {
+            Console.WriteLine("Entrada no válida.");
+        }
+
+
+        Console.WriteLine("Ingresar alumno: ");
+        bool res = true;
+
+        do
+        {
+            string name = alumno.addName();
+            List<float> grades = alumno.addGrade(numPruebas);
+            alumnos.Add(new Alumno { Name = name, grades = grades });
+
+
+            Console.Write("Desea agregar otro alumno? 1.Si / 2.No: ");
+            var input3 = Console.ReadLine();
+
+            if (input3 == "2")
+            {
+                res = false;
+            }
+        } while (res);
+
+
+        foreach (Alumno Ialumno in alumnos)
+        {
+            Console.WriteLine($"Alumno: {Ialumno.Name}.");
+            Console.WriteLine($"Notas: {string.Join(", ", Ialumno.grades)}");
+            Console.WriteLine($"Promedio: {Ialumno.GetAverage():F2}");
+            Console.WriteLine($"Estado: {(Ialumno.GetAverage() >= 6 ? "Aprobado": "Desaprobado")}");
+        }
+    }
+}
